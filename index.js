40d09749de705b66c87ec1bd42e14de425eed1f1
M.Lalikth kumar <121875934+Lalithkumar-19@users.noreply.github.com>
2024-02-03 21:59:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.post("/login", async(req, res) => {
             jwt.sign({ username, id: UserDoc._id }, secret, {}, (err, token) => {
                 if (err) {
                     throw err;
-                    res.status(400).json('wrong password entered');
+                    res.status(400).json('wrong password ,while token evaluation');
                 } else {
                     res.status(200).cookie('token', token).json({
                         id: UserDoc._id,
